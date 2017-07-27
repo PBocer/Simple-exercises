70e3a2ad8cc6abb1a29d24b456a30d8a7ac2659f
PBocer <przemek.bocer@gmail.com>
2017-07-27 15:53:33 +0200

    The beginning of a new project

--- src/conditionaStatement/scExe1.java
+++ src/conditionaStatement/scExe1.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 package conditionaStatement;
 
 /**
  * Created by Johny on 27.07.2017.
  */
 public class scExe1 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int input = in.nextInt();
+
+        if (input > 0)
+        {
+            System.out.println("Number is positive");
+        }
+        else if (input < 0)
+        {
+            System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is zero");
+        }
+    }
 }
