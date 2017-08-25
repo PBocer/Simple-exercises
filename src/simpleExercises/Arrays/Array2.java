80d350107b45851df4f48ea1cfdc57d26b96fdaf
PBocer <przemek.bocer@gmail.com>
2017-08-25 15:07:13 +0200

    Add new task related with array

---
+++ src/simpleExercises/Arrays/Array2.java
@@ -0,0 +1,18 @@
+package simpleExercises.Arrays;
+
+/**
+ Write a Java program to sum values of an array.
+ */
+public class Array2 {
+    public static void main(String[] args) {
+
+        int[]myArray={1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+
+
+        for(int i: myArray)
+            sum += i;
+        System.out.println("Sum= "+ sum);
+
+    }
+}
