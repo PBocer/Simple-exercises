e651e9731ce077180f16aaa2580b037c3a503b7b
PBocer <przemek.bocer@gmail.com>
2018-05-22 20:08:26 +0200

    Builder

---
+++ simpleExercises/src/main/java/simpleExercises/designPattern/Builder/Main.java
@@ -0,0 +1,11 @@
+package simpleExercises.designPattern.Builder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas", "Los Angeles").price(50).build();
+
+        System.out.println(leg);
+
+    }
+}
